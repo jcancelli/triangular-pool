85abd34177a6548583454ec50215fc1999c731f6
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-03 18:00:28 +0200

    Add operator<< to LinearEquation

--- src/linear_equation.cpp
+++ src/linear_equation.cpp
@@ -3,3 +3,7 @@
 double LinearEquation::operator()(double x) const {
   return gradient * x + offset;
 }
+
+std::ostream& operator<<(std::ostream& os, LinearEquation const& equation) {
+  return os << "y = " << equation.gradient << " * x + " << equation.offset;
+}
--- src/linear_equation.hpp
+++ src/linear_equation.hpp
@@ -1,10 +1,14 @@
 #ifndef LINEAR_EQUATION_HPP
 #define LINEAR_EQUATION_HPP
 
+#include <ostream>
+
 struct LinearEquation {
   double gradient;
   double offset = 0.0;
   double operator()(double x) const;
 };
+
+std::ostream& operator<<(std::ostream& os, LinearEquation const& equation);
 
 #endif
