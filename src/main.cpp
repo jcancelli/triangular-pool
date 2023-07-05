4511eae2b5dbce4bfba6ae8bb4a4be9aeb35da2a
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-05 21:07:54 +0200

    Improve main function structuring

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,39 @@
-
 #include <iostream>
 
-int main() {
+#include "arguments.hpp"
+#include "simulation.hpp"
+
+int main(int argc, char* argv[]) {
+  try {
+    Config config;
+
+    switch (parseArguments(argc, argv, config)) {
+      case ParseStatus::HelpShown:
+        return EXIT_SUCCESS;
+      case ParseStatus::Error:
+        return EXIT_FAILURE;
+      default:
+        // do nothing, keep going
+        break;
+    }
+
+    Simulation simulation(config.r1, config.r2, config.l, config.meanInitialY,
+                          config.stddevInitialY, config.meanInitialTheta,
+                          config.stddevInitialTheta);
+
+    if (config.graphics) {
+    } else {
+      for (int i = 0; i < config.iterations; i++) {
+        simulation.newIteration();
+        simulation.immediate();
+      }
+    }
+  } catch (std::exception const& e) {
+    std::cout << "Unexpected error\n";
+    std::cout << e.what() << "\n";
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cout << "Unknown error\n";
+    return EXIT_FAILURE;
+  }
 }
