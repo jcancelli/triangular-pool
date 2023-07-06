6bab62b0eeb2b7fc5042a9fd01fd2f0471f53e60
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-06 15:38:35 +0200

    Add path to Lucinda Sans Regular font to config

--- src/graphics/font.cpp
+++ src/graphics/font.cpp
@@ -12,15 +12,14 @@ struct FontConfig {
   sf::Font& variable;
 };
 
-static const std::vector<FontConfig> FONT_CONFIGS{
-    {"Lucinda Sans Regular", "", g_FontLucindaSansRegular}  //
-};
-
 void loadFonts() {
+  const std::vector<FontConfig> FONT_CONFIGS{{"Lucinda Sans Regular",
+                                              "resources/fonts/lucinda_sans_regular.ttf",
+                                              g_FontLucindaSansRegular}};
   for (auto const& font : FONT_CONFIGS) {
     if (!font.variable.loadFromFile(font.filePath)) {
-      throw std::runtime_error(std::string("Error: Could not find \"") + font.name +
-                               "\" font at path: \"" + font.filePath + "\"");
+      throw std::runtime_error(std::string("Error: Could not load font \"") + font.name +
+                               "\" from path: \"" + font.filePath + "\"");
     }
   }
 }
