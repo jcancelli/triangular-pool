f04aa45221aaa7fb1d654907150c815e6a3d8776
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-06 10:18:41 +0200

    Update main to use GraphicsWindow

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 #include "arguments.hpp"
-#include "graphics/simulation.hpp"
+#include "graphics/window.hpp"
 #include "simulation.hpp"
 
 int main(int argc, char* argv[]) {
@@ -24,8 +24,8 @@ int main(int argc, char* argv[]) {
                           config.stddevInitialTheta);
 
     if (config.graphics) {
-      SimulationGraphics graphics(700, 700, simulation);
-      graphics.show();
+      GraphicsWindow window(700, 700, simulation);
+      window.show();
     } else {
       for (int i = 0; i < config.iterations; i++) {
         simulation.newIteration();
