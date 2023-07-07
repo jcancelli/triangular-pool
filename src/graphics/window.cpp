2f9a76e415f57c0cd4f8968bf6a29b687f729606
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-07 23:57:20 +0200

    Add framerate limiter to window

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -3,10 +3,11 @@
 #define WINDOW_SIZE 700
 #define WINDOW_PADDING 200
 
-GraphicsWindow::GraphicsWindow(Simulation const& simulation)
+GraphicsWindow::GraphicsWindow(Simulation& simulation)
     : m_Window(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE), "Triangular Pool",
                sf::Style::Close | sf::Style::Titlebar, sf::ContextSettings(0, 0, 8)),
-      m_SimulationGraphics(simulation) {
+      m_SimulationGraphics(simulation),
+      m_Simulation(simulation) {
   m_SimulationGraphics.setPosition(WINDOW_PADDING, WINDOW_SIZE / 2);
   double maxCoordinate =
       std::max(simulation.getL(), std::max(simulation.getR1(), simulation.getR2()));
@@ -14,15 +15,32 @@ GraphicsWindow::GraphicsWindow(Simulation const& simulation)
 }
 
 void GraphicsWindow::show() {
+  bool redraw = true;
+
+  const double IDEAL_FRAMETIME = 1.0 / 60.0;
+
   while (m_Window.isOpen()) {
-    processEvents();
-    m_Window.clear(sf::Color(0x999999FF));
-    m_Window.draw(m_SimulationGraphics);
-    m_Window.display();
+    const sf::Time elapsed = m_Clock.getElapsedTime();
+
+    if (elapsed.asSeconds() >= IDEAL_FRAMETIME) {
+      redraw = true;
+      m_Simulation.update(elapsed.asMilliseconds());
+      m_SimulationGraphics.update(elapsed.asMilliseconds());
+      m_Clock.restart();
+    }
+
+    processWindowEvents();
+
+    if (redraw) {
+      redraw = false;
+      m_Window.clear(sf::Color(0x999999FF));
+      m_Window.draw(m_SimulationGraphics);
+      m_Window.display();
+    }
   }
 }
 
-void GraphicsWindow::processEvents() {
+void GraphicsWindow::processWindowEvents() {
   sf::Event event;
   while (m_Window.pollEvent(event)) {
     switch (event.type) {
--- src/graphics/window.hpp
+++ src/graphics/window.hpp
@@ -9,15 +9,17 @@
 class GraphicsWindow {
  private:
   sf::RenderWindow m_Window;
+  sf::Clock m_Clock;
   SimulationGraphics m_SimulationGraphics;
+  Simulation& m_Simulation;
 
  public:
-  GraphicsWindow(Simulation const& simulation);
+  GraphicsWindow(Simulation& simulation);
 
   void show();
 
  private:
-  void processEvents();
+  void processWindowEvents();
 };
 
 #endif
