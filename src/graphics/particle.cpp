1b7d54213a46745621a9641122bf84f63a615f58
jcancelli <jonathan.cancelli.46@gmail.com>
2023-07-08 11:23:48 +0200

    Set ParticleGraphics velocity line length to fixed length

--- src/graphics/particle.cpp
+++ src/graphics/particle.cpp
@@ -1,15 +1,14 @@
 #include "particle.hpp"
 
-#include <iostream>
 #include <sstream>
 
 #include "../math/angles.hpp"
 
-static const double VELOCITY_LINE_THICKNESS = 2.5;
 static const double POINT_RADIUS = 5;
 
 ParticleGraphics::ParticleGraphics(Particle const& particle) : m_Particle(particle) {
-  m_VelocityLine.setOrigin(0, VELOCITY_LINE_THICKNESS / 2);
+  m_VelocityLine.setOrigin(0, 1.25);
+  m_VelocityLine.setSize(sf::Vector2f(40, 2.5));
   m_VelocityLine.setFillColor(sf::Color::Green);
   m_Point.setRadius(POINT_RADIUS);
   m_Point.setFontSize(11);
@@ -48,8 +47,6 @@ void ParticleGraphics::updateText() {
 void ParticleGraphics::updateVelocityLine() {
   auto theta = m_Particle.getTheta();
   auto pos = m_Particle.getPosition();
-  m_VelocityLine.setSize(
-      sf::Vector2f(m_Particle.getSpeed() * getCoordsScaling(), VELOCITY_LINE_THICKNESS));
   m_VelocityLine.setRotation(degrees(-theta));
   m_VelocityLine.setPosition(pos.x * getCoordsScaling(), -pos.y * getCoordsScaling());
 }
